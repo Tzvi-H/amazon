2369e10650a67847eec1f90237158bef20c77d49
Tzvi-H <16guitar@gmail.com>
2022-07-06 08:54:01 -0600

    separated into 2 function

--- amazon-macro.js
+++ amazon-macro.js
@@ -1,20 +1,28 @@
+// The following code does the following
+//  - updates the quantity
+//  - "clicks" the add to cart button
+//  - waits a few seconds and "clicks" the no warranty button if it shows up
 (() => {
-  document.querySelector("#quantity").value = "2";
+  document.querySelector("#quantity").value = "2"; // this controls how much quantity to select
   document.querySelector("#add-to-cart-button").click();
   setTimeout(
-    () =>
-      document
-        .querySelector('input[aria-labelledby="attachSiNoCoverage-announce"]')
-        .click(),
-    2000
+    () => {
+      const input = document.querySelector(
+        'input[aria-labelledby="attachSiNoCoverage-announce"]'
+      );
+      input && input.click();
+    },
+    3000 // this controls how many seconds to wait from when you enter the code until the the "no warranty" button is clicked, (1000 equals 1 second)
   );
-  setTimeout(
-    () =>
-      document
-        .querySelector(
-          'input[aria-labelledby="attach-sidesheet-checkout-button-announce"]'
-        )
-        .click(),
-    5000
+})();
+
+// The following will "click" the checkout button
+(() => {
+  const input = document.querySelector(
+    'input[aria-labelledby="attach-sidesheet-checkout-button-announce"]'
   );
+  (input && input.click()) ||
+    document
+      .querySelector('input[data-feature-id="proceed-to-checkout-action"]')
+      .click();
 })();
